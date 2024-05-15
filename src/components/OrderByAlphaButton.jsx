06a18b3dc61cc8917be95a7d44bdd5d99a2a6e9b
emmaanord <128476191+emmaanord@users.noreply.github.com>
2024-05-15 18:53:09 +0200

    Lite CSS-justeringar

--- src/components/OrderByAlphaButton.jsx
+++ src/components/OrderByAlphaButton.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// Komponent för alfabetisk sortering, knapp stylead med bootstrap.
 const OrderByAlphaButton = ({ sortMovies }) => {
   return (
     <button className="btn btn-secondary mt-3" onClick={sortMovies}>Sortera alfabetiskt</button>
--- src/components/OrderByGradeButton.jsx
+++ src/components/OrderByGradeButton.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// Komponent för betygssortering, knapp stylead med bootstrap.
 const OrderByGradeButton = ({ sortMovies }) => {
   return (
     <button className="btn btn-secondary mt-3" onClick={sortMovies}>Sortera efter betyg</button>
